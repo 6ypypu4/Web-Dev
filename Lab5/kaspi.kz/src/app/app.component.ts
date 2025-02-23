4bcb8715524d3e762e6080c0f8b8c203d9662f4d
6ypypu4 <konovalovila272@gmail.com>
2025-02-23 21:33:40 +0500

    lab5

--- Lab5/kaspi.kz/src/app/app.component.ts
+++ Lab5/kaspi.kz/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppService } from './app.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: `app.component.html`,
+  styleUrl: `app.component.css`
 })
 export class AppComponent {
-  title = 'kaspi.kz';
+  categories: string[];
+
+  constructor(private appService: AppService) {
+    this.categories = this.appService.getCategories();
+  }
 }
