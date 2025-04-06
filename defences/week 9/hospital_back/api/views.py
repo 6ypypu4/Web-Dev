d845107ddefc90699518a372b2137ce7c46c466a
6ypypu4 <konovalovila272@gmail.com>
2025-04-06 20:19:39 +0500

    Lab8 defence

---
+++ defences/week 9/hospital_back/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import PatientSerializer
+from .models import Patient
+
+class PatientList(generics.ListCreateAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class PatientDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+# Create your views here.
